87b5914d87d9ac00ca4ddd5fc6f2445b3e3d2f83
Michael Anderson <mandersonisd@gmail.com>
2024-08-18 18:05:19 -0600

    Implemented asset caching in src-sw.js

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,14 +1,11 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
 precacheAndRoute(self.__WB_MANIFEST);
-
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -18,13 +15,28 @@ const pageCache = new CacheFirst({
     }),
   ],
 });
-
-warmStrategyCache({
-  urls: ['/index.html', '/'],
+offlineFallback({
+  pageFallback: "/index.html",
+  pageFallbackDependencies: ["/index.html"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+warmStrategyCache({
+  urls: ["/index.html", "/"],
+  strategy: pageCache,
+});
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// This register route is used to cache the assets.
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
